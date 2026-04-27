c0a4dd1247577c0242ca2bcb4ade3489b56763ed
Leo-N-1 <lebelleguic.leo@sfr.fr>
2026-04-27 13:23:00 +0200

    Add files via upload

---
+++ TestInscriptionDAO/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestInscriptionDAO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestInscriptionDAO")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5776fb4-ed0a-4180-9841-3f6bf46de213")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestInscriptionDAO/TestInscriptionDAO.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BiblioICGO;
+using BiblioICGODAO;
+
+namespace TestInscriptionDAO
+{
+    [TestClass]
+    public class TestInscriptionDAO
+    {
+        [TestMethod]
+        public void TestAjouterInscription()
+        {
+           /* Connexion.OuvrirConnexion();
+            Session uneSession = SessionDAO.GetSession("BUR", 1, 1);
+            Stagiaire unStagiaire = StagiaireDAO.GetStagiaire(2);
+            Inscription uneInscription = new Inscription(uneSession, unStagiaire, "p");
+            InscriptionDAO.AjouterUneInscription(uneInscription);
+            Connexion.FermerConnexion(); */
+        }
+
+        [TestMethod]
+        public void TestConfirmerInscription()
+        {
+            /* Connexion.OuvrirConnexion();
+            Session uneSession = SessionDAO.GetSession("BUR", 1, 1);
+            Stagiaire unStagiaire = StagiaireDAO.GetStagiaire(2);
+            Inscription uneInscription = new Inscription(uneSession, unStagiaire, "d");
+            InscriptionDAO.ConfirmerInscription(uneInscription);
+            Connexion.FermerConnexion(); */
+        }
+
+        [TestMethod]
+        public void TestSupprimerInscription()
+        {
+            /* Connexion.OuvrirConnexion();
+            InscriptionDAO.SupprimerUneInscription("BUR", 1, 1, 2);
+            Connexion.FermerConnexion(); */
+        }
+
+        [TestMethod]
+        public void TestVerifierPlacesDispo()
+        {
+            /* Connexion.OuvrirConnexion();
+            Boolean dispo = InscriptionDAO.VerifierPlacesDisponibles("BUR", 1, 1);
+            Assert.AreEqual(dispo, true);
+            Connexion.FermerConnexion(); */
+        }
+    }
+}
